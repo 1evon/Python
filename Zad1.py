42845c8fa11ede746e4b384c149bf48baa76edc8
1evon <levadmin@mail.ru>
2024-04-23 11:20:08 +0300

    init

---
+++ Zad1.py
@@ -0,0 +1,23 @@
+#1
+a = int(input("Введите число а:"))
+b = int(input("Введите число b:"))
+c = int(input("Введите число c:"))
+s = a+b+c
+u = a*b*c
+sa = (a+b+c)/3
+print ("Сумма чисел:",s, "Произведение чисел:",u, "Среднее арифметическое:",sa )
+
+#3
+from random import randint
+x= randint(100,999)
+x1 = x//100 
+x2 = (x//10)%10 
+x3 = x%10 
+print(x1,x2,x3)
+
+#2
+a = float(input("Введите координату А:"))
+b = float(input("Введите координату В:"))
+
+
+
