2f77ed6cd9e0d101253d34770a4c02ffcfdbc755
1evon <levadmin@mail.ru>
2024-04-24 10:43:12 +0300

    init

--- FIO.py
+++ FIO.py
@@ -7,3 +7,10 @@ o = r[2]
 ii = i[:1]
 oo = o[:1]
 print(ii+'.',oo+".",f)
+
+#replace
+#(X > 0) and (Y < X) and (Z > Y) and (Z <> 5)
+stroka = input("Введите :")
+sim = stroka.find('and')
+ns = stroka.replace("and","&")
+print(ns)
