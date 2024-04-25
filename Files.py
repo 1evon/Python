71b7d596e7cb972d60d2ec285fb178b1c41e1783
1evon <levadmin@mail.ru>
2024-04-25 13:06:23 +0300

    init

--- Files.py
+++ Files.py
@@ -2,6 +2,7 @@ import json
 with open('js.json', encoding = 'UTF-8') as file:
     data = json.load(file)
 print(data['name'])
-string = '{"number":123456789}'
-data = json.loads(string)
+for i in range(1, 10): 
+    string = '{"number":'+str(i)+'}'
+    data = json.loads(string)
 print(data["number"])
